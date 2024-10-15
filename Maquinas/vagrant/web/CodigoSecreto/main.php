7b30f350f1bf5716b922bd955c118ac13c7c46fc
dam2 <ggutierrezs01@iescastelar.com>
2024-10-15 14:14:16 +0200

    Commit s/auto

--- Maquinas/vagrant/web/CodigoSecreto/main.php
+++ Maquinas/vagrant/web/CodigoSecreto/main.php
@@ -1,106 +1,136 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Código Secreto</title>
-  
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-
 <?php
-$ficheroPalabras = fopen("palabras.txt", "r") or die("Imposible abrir");
-//echo fread($ficheroPalabras,filesize("palabras.txt"));
+session_start();
 
-/*$array = explode("\n",$ficheroPalabras);
-foreach($array as $palabra => $valor){
-    $valor = trim($palabra);    
+$NOMBREFICHERO = "palabras.txt";
+$celda = NULL;
+$celdacolor = NULL;
+$celdavisible = NULL;
+
+if (!isset($_SESSION["celda"]) || (isset($_GET["resetea"]))) {
+    creaTablero();
+} else {
+    $celda = $_SESSION["celda"];
+    $celdacolor = $_SESSION["celdacolor"];
+    $celdavisible = $_SESSION["celdavisible"];
 }
-*/
-while(!feof($ficheroPalabras)){
-    $array[] = trim(fgets($ficheroPalabras));
+
+if (isset($_GET["fila"]) && isset($_GET["col"])) {
+    $fila = $_GET["fila"];
+    $col = $_GET["col"];
+    darVuelta($fila, $col);
 }
 
 
-$tablero = [];
-for ($w = 0; $w<5; $w++){
-    for ($t = 0; $t<5; $t++){
-        $tablero[$w][$t] =  $array[rand(0,401)];
+function creaTablero() {
+    global $NOMBREFICHERO;
+    global $celda;
+    global $celdacolor;
+    global $celdavisible;
+
+// Abre el fichero y carga la lista de palabras
+    $myfile = fopen($NOMBREFICHERO, "r") or die("Unable to open file!");
+    while(!feof($myfile)) {
+        $listaPalabras[] = trim(fgets($myfile));
+      }
+    fclose($myfile);
+    
+// Ordena aleatoriamente las palabras    
+    shuffle($listaPalabras);
+    
+// Ordena aleatoriamente los colores
+    $colores = ['white','white','white','white','white','white','white','white','white','white','white', 
+    'danger','danger','danger','danger','danger','danger',
+    'black',
+    'success','success','success','success','success','success','success'];
+    shuffle($colores);
+
+
+    $contador=0;
+    for ($fila=1; $fila<=5; $fila++){
+        for ($col=1; $col<=5; $col++){
+            $celda[$fila][$col] = $listaPalabras[$contador];
+            $celdacolor[$fila][$col] = $colores[$contador];
+            $celdavisible[$fila][$col] = FALSE;
+            $contador++;
+        }
     }
+
+    $_SESSION["celda"] = $celda;
+    $_SESSION["celdacolor"] = $celdacolor;
+    $_SESSION["celdavisible"] = $celdavisible;
 }
 
+function darVuelta($fila, $columna) {
+    global $celdavisible;
+    $celdavisible[$fila][$columna] = True;
+    $_SESSION["celdavisible"] = $celdavisible;
+}
 
+function resuelveTablero(){
+    global $celda;
+    global $celdacolor;
 
-$colores = ['white', 'danger', 'black','primary'];
-$danger = 7;
-$primary = 6;
-$black = 1;
-$white = 11;
-
-
-echo "<table class='table table-bordered'>"; 
-//while(($danger+$primary+$black+$white)>0){
-    for ($w = 0; $w<5; $w++){
+    echo "<table border=1>";
+    for ($fila=1; $fila<=5; $fila++){
         echo "<tr>";
-        for ($t = 0; $t<5; $t++){
-                //$color = $colores[rand(0,(count($colores) - 1))];
-                $color = $colores[array_rand($colores)];
-                echo "<td class='bg-$color' style='width: 10px; height: 50px;'>" . $tablero[$w][$t]  . "</td>";                
-                $$color--;
-                if($$color<=0){
-                    unset($colores[array_search($color,$colores)]);
-                }    
-            } 
-    }    
-echo "</tr>";
-//}
-echo "</table>";
-echo "<br><br>";
-echo "<br><br>";
-fclose($ficheroPalabras);
-//var_dump($array);
-?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-
-
-
-
-
-
-
-<?php
-/*$colores = ['blue', 'red', 'white','green'];
-$red = 0;
-$blue = 0;
-$green = 0;
-
-
-
-echo "<table class='table-active table-borderer'>";
-echo "<table class='table table-bordered'>"; 
-for ($w = 0; $w<5; $w++){
-        echo "<tr>";
-        for ($t = 0; $t<5; $t++){
-            $color = $colores[rand(0,3)];
-            echo "<td bgcolor = " . $color . ">" . $tablero[$w][$t]  . "</td>";                
-            $$color++;
+        for ($col=1; $col<=5; $col++){         
+            echo "<td class='bg-" . $celdacolor[$fila][$col] . "'>";
+            echo "<b>";        
+            echo "<a href='?fila=" . $fila . "col=" . $col . "'>"; 
+            echo $celda[$fila][$col];
+            echo "</a>";
+            echo "</b>"; 
+            echo "</td>";
         }
         echo "</tr>";
+    }
+    echo "</table>";
+
 }
-echo "</table>";
-echo $red ;
-echo "<br><br>";
-echo $blue ;
-echo "<br><br>";
-echo $green ;
-fclose($ficheroPalabras);
-//var_dump($array);
+
+function dibujaTablero(){
+    global $celda;
+    global $celdacolor;
+    global $celdavisible;
+
+    echo "<table border=1>";
+    for ($fila=1; $fila<=5; $fila++){
+        echo "<tr>";
+        for ($col=1; $col<=5; $col++){         
+            if ($celdavisible[$fila][$col]) {
+                echo "<td class='bg-" . $celdacolor[$fila][$col] . "'>";
+            } else {
+                echo "<td bgcolor='orange'>";
+            }
+            echo "<b>";        
+            echo "<a href='?fila=" . $fila . "&col=" . $col . "'>"; 
+            echo $celda[$fila][$col];
+            echo "</a>";
+            echo "</b>"; 
+            echo "</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+
+}
 ?>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-*/
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CodigoSecreto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-
+</head>
+<body>
+    <?php
+        dibujaTablero();
+    ?>
+    <hr>
+    <a href="?resetea=1">Resetear</a>
+</body>
+</html>
