9f371750e6a32dda51253e30600f59da523e2b07
gineeesss <ggutierrezs01@iescastelar.com>
2024-11-06 13:26:34 +0100

    Commit s/auto

---
+++ Temp/empresas/app/Http/Controllers/PruebaController.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\http\Request;
+use App\Models\Empleado;
+use App\Models\Empresa;
+
+class PruebaController extends Controller
+{
+public function buscaempleado($pempresa, $pempleado) {
+    $empresa = Empleado::find($pempresa);
+    return ($empresa->empleados[$pempleado]->nombre);
+}
+    public function verempleado($pempleado) {
+    $empleado = Empleado::find($pempleado);
+    $respuesta = $empleado->nombre . " (" . $empleado->empresa->nombre . ")";
+    return $respuesta;
+}
+public function verempresas() {
+    $empresas= Empresa::all();
+    $respuesta="";
+    foreach ($empresas as $empresa) {
+        $respuesta .= "$empresa->nombre<br>";
+    }
+    return $respuesta;
+}
+}
